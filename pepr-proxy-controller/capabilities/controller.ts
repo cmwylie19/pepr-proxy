2b24aae1fb9afd193057a006c0d489caae54b68f
Case Wylie <cmwylie19@defenseunicorns.com>
2023-07-28 15:30:55 -0400

    debugging
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- pepr-proxy-controller/capabilities/controller.ts
+++ pepr-proxy-controller/capabilities/controller.ts
@@ -1,22 +1,18 @@
 import {
   Capability,
   Log,
-  PeprRequest,
   RegisterKind,
   a,
-  fetch,
-  fetchStatus,
-  k8s,
 } from "pepr";
-import {WatchedPods, Gateway, GatewayAttributes } from "../lib/Gateway"
-import {K8sAPI, createContainer} from "../lib/kubernetes-api"
+import { Gateway, GatewayAttributes } from "./lib/gateway"
+import {K8sAPI, createContainer} from "./lib/kubernetes-api"
 /**
  *  The HelloPepr Capability is an example capability to demonstrate some general concepts of Pepr.
  *  To test this capability you run `pepr dev`and then run the following command:
  *  `kubectl apply -f capabilities/hello-pepr.samples.yaml`
  */
 export const Controller = new Capability({
-  name: "gateway-controller",
+  name: "controller",
   description: "Controller for edge gateway.",
   namespaces: [],
 });
@@ -32,28 +28,26 @@ RegisterKind(Gateway, {
   kind: "Gateway",
 });
 
+
 When(Gateway)
   .IsCreatedOrUpdated()
   .Then(async gw => {
-
+    Log.info("Saw a Gateway object ", JSON.stringify(gw, undefined, 2))
     proxies[gw.Raw?.metadata?.name] = {
       server: gw.Raw?.spec?.server,
       jwtAuth: gw.Raw?.spec?.jwtAuth,
       rateLimit: gw.Raw?.spec?.rateLimit
     }
 
-    // find & roll pods meeting said labels
     await k8sAPI.findAndDeletePods({"proxy":gw.Raw?.metadata?.name})
-
   })
+
   When(a.Pod)
   .IsCreatedOrUpdated()
   .Then(async pod => {
     if (pod.Raw?.metadata?.labels?.["proxy"] !== undefined) {
       pod.Raw?.spec?.containers.push(createContainer(proxies[pod.Raw?.metadata?.labels?.["proxy"]]))
     }
-
     await k8sAPI.createService(pod.Raw?.metadata?.name+"-proxy",pod.Raw?.metadata?.namespace,{"proxy":pod.Raw?.metadata?.labels?.["proxy"]},proxies[pod.Raw?.metadata?.labels?.["proxy"]].server.port)
-
   })
 
